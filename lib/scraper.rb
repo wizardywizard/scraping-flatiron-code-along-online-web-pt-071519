c2f134a90851e4691cbdebae2eb425e0be666a2e
David Little <littledavid175@gmail.com>
2019-08-19 17:42:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,12 +7,6 @@ class Scraper
   
    def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-    #   doc.css(".post").each do |post|
-    #     course = Course.new
-    #     course.title = post.css("h2").text
-    #     course.schedule = post.css(".date").text
-    #     course.description = post.css("p").text
-    # end
   end
   
   def get_courses
@@ -40,6 +34,6 @@ end
   end
   
 end
-
+Scraper.new.print_courses
 
 
